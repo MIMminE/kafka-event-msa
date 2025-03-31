acc23a968072dfb94ecb959bd18bdee2628278f5
MIMminE <weluv.ww@gmail.com>
2025-03-31 16:56:51 +0900

    [ kafka-modules ] 카프카 관련 모듈 커밋 - avro 라이브러리 사용을 위한 설정

---
+++ kafka/src/main/java/dev/nuts/App.java
@@ -0,0 +1,13 @@
+package dev.nuts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
