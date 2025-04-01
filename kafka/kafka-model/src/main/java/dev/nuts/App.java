a317d92c57e733eed9216474ec35bcf3e87f9351
MIMminE <weluv.ww@gmail.com>
2025-04-01 10:58:18 +0900

    [ kafka-model ] Avro 라이브러리 설정

--- kafka/kafka-model/src/main/java/dev/nuts/App.java
+++ kafka/kafka-model/src/main/java/dev/nuts/App.java
@@ -1,5 +1,7 @@
 package dev.nuts;
 
+import dev.nuts.kafka.avro.model.StreamAvroModel;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 }
